d8ad92f12a00bf156f91883319c416ec33ee19f7
Nika Jokhadze <littlekoku@gmail.com>
2018-12-23 01:31:38 +0400

    score updating and active players changing

--- app.js
+++ app.js
@@ -35,9 +35,22 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 	//3.Update the roundscore if the rolled number is NOT 1
 	if(dice !== 1 ) {
-		document.querySelector('#current-' + activePlayer).textContent = dice;
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	} else {
-		document.querySelector('#current-' + activePlayer).textContent = 0;
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		diceDOM.style.display = 'none';
+		//document.querySelector('.player-0-panel').classList.remove('active');
+		//document.querySelector('.player-1-panel').classList.add('active');
+		
 	}
 
 })
